74afe95c92562d7354e044254fc39ba59e0afcff
Miguel <miaortizma@unal.edu.co>
2020-03-12 15:56:14 -0500

    new family of images

---
+++ tensorflow-cuda-based/dqn-image/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+BUILD_ARGS="--build-arg http_proxy=http://proxy4.unal.edu.co:8080 --build-arg https_proxy=http://proxy4.unal.edu.co:8080"
+sudo docker build $BUILD_ARGS -t miaortizma/jupyter:dqn-gpu .
